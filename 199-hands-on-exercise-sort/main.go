5ccb5258981f98b7c3188fd61e106a5de1ef69aa
Gaurav Chitlangia <gauravchitlangia.14@gmail.com>
2025-02-12 15:49:41 +0530

    json * sort

--- 199-hands-on-exercise-sort/main.go
+++ 199-hands-on-exercise-sort/main.go
@@ -13,7 +13,7 @@ type user struct {
 }
 
 func main() {
-	/*xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
 	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
 
 	fmt.Println(xi)
@@ -25,7 +25,7 @@ func main() {
 	// sort xs
 	sort.Strings(xs)
 	fmt.Println(xs)
-	*/
+
 	u1 := user{
 		First: "James",
 		Last:  "Bond",
